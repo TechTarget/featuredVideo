0a690e78375007a0ec958ca49b3ac8e5c1cfd5fb
Morgan Wigmanich <okize123@gmail.com>
2013-01-21 20:12:48 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+Featured Video microsite component
+
+Brightcove documentation:
+  http://support.brightcove.com/en/video-cloud/docs/using-smart-player-api
+
+Need to set this in Brightcove control panel:
+  videocloud manager > player settings > web settings > enable actionscript/javascript apis
---
+++ featuredVideo.js
@@ -0,0 +1,53 @@
+/*!
+* Featured Video v0.0.1 (http://okize.github.com/)
+* Copyright (c) 2013 | Licensed under the MIT license - http://www.opensource.org/licenses/mit-license.php
+*/
+
+// use AMD or browser globals to create a jQuery plugin.
+;(function (factory) {
+
+  if (typeof define === 'function' && define.amd) {
+    define(['jquery'], factory);
+  } else {
+    factory(jQuery);
+  }
+
+}(function ($) {
+
+  'use strict';
+
+  // defaults
+  var pluginName = 'featuredVideo';
+  var defaults = {
+    foo: 'bar'
+  };
+
+  // plugin constructor
+  function Plugin( element, options ) {
+    this.el = $(element);
+    this.options = $.extend( {}, defaults, options) ;
+    this._defaults = defaults;
+    this._name = pluginName;
+    this.init();
+  }
+
+  Plugin.prototype = {
+
+    init: function() {
+      console.log('featuredVideo plugin working');
+    }
+
+  };
+
+  // A really lightweight plugin wrapper around the constructor,
+  // preventing against multiple instantiations
+  $.fn[pluginName] = function ( options ) {
+    return this.each(function () {
+      if (!$.data(this, 'plugin_' + pluginName)) {
+        $.data(this, 'plugin_' + pluginName,
+          new Plugin( this, options ));
+      }
+    });
+  };
+
+}));
