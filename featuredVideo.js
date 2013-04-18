45bf86c8124ddf2fb4cbd69dafacf43cfd124598
Morgan Wigmanich <okize123@gmail.com>
2013-04-18 16:33:31 -0400

    fixed IE7/8 issue

--- featuredVideo.js
+++ featuredVideo.js
@@ -1,5 +1,5 @@
 /*!
-* Featured Video v1.1.4 (http://okize.github.com/)
+* Featured Video v1.0.5 (http://okize.github.com/)
 * Copyright (c) 2013 | Licensed under the MIT license - http://www.opensource.org/licenses/mit-license.php
 */
 
